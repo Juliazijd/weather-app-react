a446a24d10b5376de93b52d3fb3e9053840df4f1
Juliazijd <68395721+Juliazijd@users.noreply.github.com>
2020-11-12 15:07:43 +0100

    Added Footer with right github link, tried different location to check result

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      <a
+        href="https://github.com/Juliazijd/weather-app-react"
+        target="_blank"
+        rel="noreferrer"
+        className="source-link"
+      >
+        Open source code{" "}
+      </a>
+      by Julia Zijderveld
+    </footer>
+  );
+}
