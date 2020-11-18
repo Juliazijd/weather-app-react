b95794d566ff7392a4102a991ff7acf226d0e7b2
Juliazijd <68395721+Juliazijd@users.noreply.github.com>
2020-11-18 14:41:09 +0100

    Try to make if else work, made new component for current weather and forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Forecast from "./Forecast";
+import WeatherCurrent from "./WeatherCurrent";
+import WeatherForecast from "./WeatherForecast";
 
 import "./Weather.css";
 
-export default function SearchEngine() {
+export default function SearchEngine(props) {
   const [city, setCity] = useState(" ");
   const [weather, setWeather] = useState({});
+  const [loaded, setLoaded] = useState(false);
 
   function displayWeather(response) {
     setWeather({
       city: response.data.name,
+      //date: new Date(response.data.dt * 1000),
       description: response.data.weather.description,
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       temperature: response.data.main.temp,
@@ -18,21 +21,18 @@ export default function SearchEngine() {
       humidity: response.data.main.humidity,
       windspeed: response.data.wind.speed,
     });
+    setLoaded(true);
   }
 
   function handleSubmit(event) {
-    event.preventDefault();
-      let apiKey = "427b64eee1edb35b88796269421b55f1";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  event.preventDefault();
+}
 
-      axios.get(apiUrl).then(displayWeather);
-  }
+ function updateSubmit(event) {
+ setCity(event.target.value);
+ }
 
-  function updateSubmit(event) {
-    setCity(event.target.value);
-  }
-
+if (loaded) {
   return (
     <div className="Weather">
       <form onSubmit={handleSubmit}>
@@ -42,8 +42,8 @@ export default function SearchEngine() {
               type="search"
               placeholder="Enter any city..."
               className="search-input w-100"
-              autocomplete={false}
-              autofocus={true}
+              autoComplete={false}
+              autoFocus={true}
               onChange={updateSubmit}
             />
           </div>
@@ -59,46 +59,18 @@ export default function SearchEngine() {
           </div>
         </div>
       </form>
-      <div className="city-and-time row">
-        <div className="col-6">
-          <h3 className="city-header">
-            <strong>{weather.city}</strong>
-          </h3>
-        </div>
-        <div className="col-6">
-          <div className="last-updated-time">
-            <div className="current-time">Friyay 14:00</div>
-            <div className="text-last-updated">(last updated)</div>
-          </div>
-        </div>
-      </div>
-      <div className="CurrentWeather row">
-        <div className="col-4 current-weather">
-          <h4 className="description">{weather.description}</h4>
-          <span><img src={weather.icon} alt={weather.description}/></span>
-          <span>
-            <strong className="current-temp">
-              {Math.round(weather.temperature)}
-            </strong>
-            <span className="units">
-              <a href="/" className="active">
-                °C{" "}
-              </a>{" "}
-              | <a href="/">°F </a>
-            </span>
-          </span>
-        </div>
-        <div className="col-8">
-          <ul className="current-conditions">
-            <li>Feels like: {Math.round(weather.feelslike)}°C</li>
-            <li>Humidity: {weather.humidity}%</li>
-            <li>Windspeed: {Math.round(weather.windspeed * 1.852)}km/h</li>
-          </ul>
-        </div>
-        
-      </div>
-      <Forecast />
+      <WeatherCurrent data={weather}/>
+      <WeatherForecast/>
     </div>
   );
+} else {
+  let apiKey = "427b64eee1edb35b88796269421b55f1";
+  let units = "metric";
+  let city = "New York"
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  
+  axios.get(apiUrl).then(displayWeather);
 
+  return "Loading...";
+ }
 }
---
+++ src/WeatherCurrent.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function WeatherCurrent(props) {
+
+    return(
+        <div className="WeatherCurrent">
+          <div className="city-and-time row">
+        <div className="col-6">
+          <h3 className="city-header">
+            <strong>{props.data.city}</strong>
+          </h3>
+        </div>
+        <div className="col-6">
+          <div className="last-updated-time">
+            <div className="current-time">{props.data.date}</div>
+            <div className="text-last-updated">(last updated)</div>
+          </div>
+        </div>
+      </div>
+      <div className="CurrentWeather row">
+        <div className="col-4 current-weather">
+          <h4 className="description">{props.data.description}</h4>
+          <span><img src={props.data.icon} alt={props.data.description}/></span>
+          <span>
+            <strong className="current-temp">
+              {Math.round(props.data.temperature)}
+            </strong>
+            <span className="units">
+              <a href="/" className="active">
+                °C{" "}
+              </a>{" "}
+              | <a href="/">°F </a>
+            </span>
+          </span>
+        </div>
+        <div className="col-8">
+          <ul className="current-conditions">
+            <li>Feels like: {Math.round(props.data.feelslike)}°C</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Windspeed: {Math.round(props.data.windspeed * 1.852)}km/h</li>
+          </ul>
+        </div>
+      </div>
+      </div>
+    );
+}
